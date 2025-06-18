08bb0ca77d7ad489202a4acfcd097bb1c368e082
brunocruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-06-18 06:21:11 +0100

    First package draft (#1)
    * Add dev environment configuration files
    * Force unsafe context to circumvent memory leak issue
    * Wrap frame metadata
    * Add package metadata
    * Update XIMEA api dependencies and build from zip
    
    Due to the large number of embedded manifests in native dependencies we
    zip the component for version control storage.
    
    The project is also marked with the win-x64 runtime identifier.
    
    ---------
    
    Co-authored-by: glopesdev <g.lopes@neurogears.org>

---
+++ Bonsai.Ximea/SerialNumberConverter.cs
@@ -0,0 +1,22 @@
+﻿using xiApi.NET;
+using System.ComponentModel;
+
+class SerialNumberConverter : StringConverter
+{
+    public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+    {
+        return true;
+    }
+
+    public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+    {
+        var camEnum = new xiCamEnum();
+        var camCount = camEnum.ReEnumerate();
+        var camSerialNumber = new string[camCount];
+        for (int i = 0; i < camCount; i++)
+        {
+            camSerialNumber[i] = camEnum.GetSerialNumById(i);
+        }
+        return new StandardValuesCollection(camSerialNumber);
+    }
+}
